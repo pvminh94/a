0ad5e6f79c6f855a1f6f620369ffdaf1174db1e5
pvminh <phanvanminh0203@mail.com>
2023-04-01 14:57:07 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class="col-sm-2">
+<img src="logo\logo.jpg" class="img-fluid float-left" alt="logo">
+</div>
+<div class="col-sm-8">
+<h1 class="blue-text mb-4 font-weight-bold">Header Goes Here</h1>
+</div>
+<nav class="col-sm-2">
+<div class="btn-group-vertical btn-group-sm" role="group" aria-label="Button Group">
+    <button type="button"class="btn btn-secondary" onclick="location.href='regsiter-page.php'">Register</button>
+    <button type="button" class="btn btn-secondary" onclick="location.href = 'register-view-users.php'"> View Users</button>
+    <button type="button" class="btn btn-secondary" onclick="location.href = 'change-password.php'" >New Password</button>
+</div>
+</nav>
