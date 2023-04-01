20b8d351eece7a637b1934760d77c99bb5ac235b
pvminh <phanvanminh0203@mail.com>
2023-04-01 16:03:41 +0700

    4 commit

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -11,3 +11,4 @@ DEFINE ('DB_NAME', 'simpledb');
 $dbcon = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 // Set the encoding...optional but recommended
 $a = mysqli_set_charset($dbcon, 'utf8');
+echo $a;
