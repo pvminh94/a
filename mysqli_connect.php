306bd87f9971f00b889e1b8e57272d8e56b19ecc
pvminh94 <phanvanminh0203@gmail.com>
2023-04-02 14:18:50 +0700

    12comit

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -6,7 +6,6 @@ DEFINE ('DB_USER', 'horatio'); // or whatever userid you created
 DEFINE ('DB_PASSWORD', 'Hmsv1ct0ry'); // or whatever password you created
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'simpledb');
-$b = "hello world";
 // Make the connection:
 $dbcon = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 // Set the encoding...optional but recommended
